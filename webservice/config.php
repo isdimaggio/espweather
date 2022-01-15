fe8ea3f1fb0508496726035c44986e8713393379
Vittorio Lo Mele <vittorio@mrbackslash.it>
2022-01-15 14:50:37 +0100

    prep openday

--- webservice/config.php
+++ webservice/config.php
@@ -16,10 +16,10 @@ limitations under the License.
 */
 
 
-$db_address = "localhost";          #indirizzo database
+$db_address = "10.8.0.1";           #indirizzo database
 $db_user = "root";                  #username database
-$db_pass = "";                      #password database
-$db_name = "spring";                #nome database
+$db_pass = "root";                  #password database
+$db_name = "nextcloud";             #nome database
 $db_table = "meteo";                #nome tabella dati
 $sensor_timeout = 1200;             #dopo quanti secondi il sensore può considerarsi offline (per mandare gli allarmi)
 $enable_old_record_clear = true;    #se attivato elimina i record più vecchi di $old_record_clear_offset giorni
